ace61cbdb15c22c249d5729d7c0117f347f823d9
JT <joni.m.t@icloud.com>
2020-11-24 18:41:11 +0200

    Added unit tests for compact function

---
+++ test/compact.test.js
@@ -0,0 +1,40 @@
+import compact from '../src/compact.js'
+
+test(`Returns [1, 2, 3] with parameters: [0, 1, false, 2, '', 3]`, () => {
+    const array = [0, 1, false, 2, '', 3]
+    expect(compact(array)).toEqual([1, 2, 3]);
+});
+
+test(`Returns [1, 2, 3] with parameters: [0, 1, null, 2, "hello", 3]`, () => {
+    const array = [0, 1, null, 2, "hello", 3]
+    expect(compact(array)).toEqual([1, 2, 3]);
+});
+
+test(`Returns [1, 2, 3] with parameters: [0, 1, undefined, 2, NaN, 3]`, () => {
+    const array = [0, 1, undefined, 2, NaN, 3]
+    expect(compact(array)).toEqual([1, 2, 3]);
+});
+
+test(`Returns [1, 2, 3] with parameters: [1, 2, 3]`, () => {
+    const array = [1, 2, 3]
+    expect(compact(array)).toEqual([1, 2, 3]);
+});
+
+test(`Returns [] with parameters: [undefined, undefined, undefined]`, () => {
+    const array = [undefined, undefined, undefined]
+    expect(compact(array)).toEqual([]);
+});
+
+test(`Returns [] with parameters: []`, () => {
+    const array = []
+    expect(compact(array)).toEqual([]);
+});
+
+test(`Returns null with parameters: undefined`, () => {
+    const array = undefined
+    expect(compact(array)).toBeNull();
+});
+
+test(`Returns [] without parameters`, () => {
+    expect(compact()).toEqual([]);
+});
