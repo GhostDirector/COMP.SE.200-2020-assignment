f8e084ef5802bfd0d781e9bbb88dc6ef173844f7
Marianne <marianne.siren@gmail.com>
2020-11-26 15:23:11 +0200

    Add more tests for toNumber function

--- test/toNumber.test.js
+++ test/toNumber.test.js
@@ -4,4 +4,23 @@ test('returns a number', () => {
   expect(toNumber(2)).toEqual(2);
   expect(toNumber('2')).toEqual(2);
   expect(toNumber(Infinity)).toEqual(Infinity);
+  expect(toNumber(null)).toEqual(0);
 });
+
+test('returns a number for hexadecimal parameter', () => {
+  expect(toNumber('0x32')).toEqual(50);
+});
+
+test('returns a number for octal parameter', () => {
+  expect(toNumber('0o24')).toEqual(20);
+});
+
+test('returns a number for binary parameter', () => {
+  expect(toNumber('0b1100')).toEqual(12);
+});
+
+test('returns NaN for undefined parameter', () => {
+  expect(toNumber(undefined)).toEqual(NaN);
+  expect(toNumber('hello')).toEqual(NaN);
+});
+
