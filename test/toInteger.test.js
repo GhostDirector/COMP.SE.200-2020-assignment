0fc8a22c5b39182d4f0a3bb9fea98f965a264e6f
Marianne <marianne.siren@gmail.com>
2020-11-26 14:56:54 +0200

    Assert undefined to return 0

--- test/toInteger.test.js
+++ test/toInteger.test.js
@@ -5,4 +5,5 @@ test('returns an integer value', () => {
   expect(toInteger(Infinity)).toBe(1.7976931348623157e+308);
   expect(toInteger('3.2')).toBe(3);
   expect(toInteger(null)).toBe(0);
+  expect(toInteger(undefined)).toBe(0);
 });
