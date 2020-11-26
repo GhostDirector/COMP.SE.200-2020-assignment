3aadf9e4912a94d8bf51f86132d9b05c7d0a7440
Marianne <marianne.siren@gmail.com>
2020-11-26 14:50:51 +0200

    Assert empty string for undefined and null parameter return values

--- test/toString.test.js
+++ test/toString.test.js
@@ -4,8 +4,9 @@ test('returns a string', () => {
   expect(toString(123)).toEqual('123');
   expect(toString([1,2,3])).toEqual('1,2,3');
   expect(toString('test')).toEqual('test');
-
-  // ???
-  expect(toString(null)).toEqual('null');
-  expect(toString(undefined)).toEqual('undefined');
 });
+
+test('returns and empty string for null and undefined parameters', () => {
+  expect(toString(null)).toEqual('');
+  expect(toString(undefined)).toEqual('');
+})
