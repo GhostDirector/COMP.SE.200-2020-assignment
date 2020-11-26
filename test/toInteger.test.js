df1152f23e0485f08e4a9b10f238928121d3a438
Marianne <marianne.siren@gmail.com>
2020-11-26 14:57:50 +0200

    Change assertions

--- test/toInteger.test.js
+++ test/toInteger.test.js
@@ -2,8 +2,8 @@ import toInteger from '../src/toInteger.js'
 
 test('returns an integer value', () => {
   expect(toInteger(3.2)).toEqual(3);
-  expect(toInteger(Infinity)).toBe(1.7976931348623157e+308);
-  expect(toInteger('3.2')).toBe(3);
-  expect(toInteger(null)).toBe(0);
-  expect(toInteger(undefined)).toBe(0);
+  expect(toInteger(Infinity)).toEqual(1.7976931348623157e+308);
+  expect(toInteger('3.2')).toEqual(3);
+  expect(toInteger(null)).toEqual(0);
+  expect(toInteger(undefined)).toEqual(0);
 });
