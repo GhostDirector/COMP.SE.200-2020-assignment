695a55b8e97d2c664eae7e6b5bf7aac5c2b6a594
Marianne <marianne.siren@gmail.com>
2020-11-30 16:51:14 +0200

    Add more tests to words

--- test/words.test.js
+++ test/words.test.js
@@ -2,6 +2,17 @@ import words from '../src/words.js'
 
 test('returns words', () => {
   expect(words('fred, barney, & pebbles')).toEqual(['fred', 'barney', 'pebbles']);
-  expect(words('fred, barney, & pebbles', /[^, ]+/g)).toEqual(['fred', 'barney', '&', 'pebbles']);
   expect(words('1,2,3')).toEqual(['1','2','3']);
+});
+
+test('returns words with given pattern', () => {
+  expect(words('fred, barney, & pebbles', /[^, ]+/g)).toEqual(['fred', 'barney', '&', 'pebbles']);
+});
+
+test('returns words with undefined pattern', () => {
+  expect(words('fred, barney', undefined)).toEqual(['fred', 'barney']);
+});
+
+test('returns an empty array for empty string', () => {
+  expect(words('')).toEqual([]);
 });
