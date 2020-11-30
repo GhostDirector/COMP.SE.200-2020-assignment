2551690075687b683eff327047816213c276a270
Marianne <marianne.siren@gmail.com>
2020-11-30 16:42:53 +0200

    Add more tests to toFinite

--- test/toFinite.test.js
+++ test/toFinite.test.js
@@ -6,3 +6,10 @@ test('returns a finite', () => {
   expect(toFinite(Number.MIN_VALUE)).toEqual(5e-324);
   expect(toFinite(Infinity)).toEqual(1.7976931348623157e+308);
 });
+
+test('returns 0 with undefined values', () => {
+  expect(toFinite(undefined)).toEqual(0);
+  expect(toFinite(null)).toEqual(0);
+  expect(toFinite([])).toEqual(0);
+  expect(toFinite([1,2])).toEqual(0);
+})
